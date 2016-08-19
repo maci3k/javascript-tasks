87d696e3ce58e9afa72e7e78a469beb846da58fb
Maciej Klimczyk <jackson93.07@gmail.com>
2016-08-19 13:30:48 +0200

    Task completed

--- app/exercise2.js
+++ app/exercise2.js
@@ -5,23 +5,64 @@
     window.exercise2 = {
         reverseNumber: function (number)
         {
-
+            if(typeof number === 'number') {
+                return Number(number.toString().split('').reverse().join(''));
+            }
+            else {
+                return false;
+            }
         },
         squareOrCube: function (array)
         {
+            for(var i=0; i<array.length; i++) {
+                if(array[i] % 2 !== 0) {
+                    array[i] = Math.pow(array[i], 2);
+                }
+                else {
+                    array[i] = Math.pow(array[i], 3);
+                }
+            }
 
+            return array;
         },
         replaceString: function (list, string, newString)
         {
-
+            var counter = 0;
+            for(var i=0; i<list.length; i++) {
+                if(list[i] === string) {
+                    list[i] = newString;
+                    counter++;
+                }
+            }
+            return counter===0 ? false: list;
         },
         maxArray: function (array)
         {
+            var max = array[0];
 
+            for(var i=0; i<array.length; i++) {
+                for(var j=0; j<array.length; j++) {
+                    if(typeof array[j] !== 'number') {
+                        return false;
+                    }
+                    if(array[j] >= max) {
+                        max = array[j];
+                    }
+                }
+            }
+
+            return max;
         },
         getObject: function (list, name)
         {
-
+            var counter = 0;
+            for(var i=0; i<list.length; i++) {
+                if(list[i].name === name) {
+                    counter++;
+                    return list[i];
+                }
+            }
+            return counter===0 ? false: list;
         }
     };
 })();
