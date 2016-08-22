daf60aa7f4edba9a5848e8e3f8ddcfccea27145a
Maciej Klimczyk <jackson93.07@gmail.com>
2016-08-22 10:34:00 +0200

    Task completed

--- app/exercise4.js
+++ app/exercise4.js
@@ -4,5 +4,52 @@
 
     // You can extend prototypes here.
 
+        String.prototype.countVowels = function() {
+            var count = 0;
+
+            for(var i=0; i<this.length; i++) {
+                switch(this[i]) {
+                    case 'a': count++; break;
+                    case 'A': count++; break;
+                    case 'e': count++; break;
+                    case 'E': count++; break;
+                    case 'i': count++; break;
+                    case 'I': count++; break;
+                    case 'o': count++; break;
+                    case 'O': count++; break;
+                    case 'u': count++; break;
+                    case 'U': count++; break;
+                    case 'y': count++; break;
+                    case 'Y': count++; break;
+                }
+            }
+
+            return count;
+        };
+
+        Array.prototype.arrayToString = function() {
+            var result = '';
+
+            if(this.length > 0) {
+                result = this[0];
+            }
+
+            for(var i=1; i<this.length; i++) {
+                result += ' ' + this[i].valueOf();
+            }
+
+            return result;
+        };
+
+        Number.prototype.add = function(newNumber) {
+            return this + newNumber;
+        };
+
+        Number.prototype.isInTheRange = function(from, to) {
+            if(from > to) {
+                return undefined;
+            }
+            return (this > from && this < to);
+        };
 
 })();
