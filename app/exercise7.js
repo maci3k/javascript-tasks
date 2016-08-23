7879c1ca78e1974ad9101a5abeb2d831855930b6
Maciej Klimczyk <jackson93.07@gmail.com>
2016-08-23 12:21:58 +0200

    Task completed

--- app/exercise7.js
+++ app/exercise7.js
@@ -6,23 +6,75 @@
 
         isPrimeNumber: function (num)
         {
+            for(var i=2; i<num; i++) {
+                if(num % i === 0) {
+                    return false;
+                }
+            }
 
+            return true;
         },
         whatType: function (arg)
         {
-
+            return typeof arg;
         },
         findMin: function (arr)
         {
+            if(arr.length !== 0) {
+                var min;
 
+                for(var i=0; i<arr.length; i++) {
+                    if(typeof arr[i] === 'number') {
+                        min = arr[i];
+                        break;
+                    }
+                }
+                for(i=1; i<arr.length; i++) {
+                    if(min > arr[i] && typeof arr[i] === 'number') {
+                        min = arr[i];
+                    }
+                }
+
+                return min;
+            }
+            else {
+                return false;
+            }
         },
         findAlmostMax: function (arr)
         {
+            var max = [];
 
+            for(var i=0; i<arr.length; i++) {
+                if(typeof arr[i] === 'number') {
+                    max.push(arr[i]);
+                }
+            }
+
+            if(max.length === 0) {
+                return false;
+            }
+            else {
+                max = max.sort();
+                return max[max.length-2];
+            }
         },
         findAlmostMin: function (arr)
         {
+            var min = [];
 
+            for(var i=0; i<arr.length; i++) {
+                if(typeof arr[i] === 'number') {
+                    min.push(arr[i]);
+                }
+            }
+
+            if(min.length === 0) {
+                return false;
+            }
+            else {
+                return min.sort()[1];
+            }
         }
     };
 })();
